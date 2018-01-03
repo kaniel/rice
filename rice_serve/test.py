0915c71b3aad6f39f93db4c4f74301947dae86f2
kaniel <378101078@qq.com>
2018-01-03 17:21:23 +0800

    set mui channel

--- rice_serve/test.py
+++ rice_serve/test.py
@@ -11,10 +11,12 @@ apiKey = "35be4183-e008-4c67-a338-e266d1884f4b"
 secretKey = "B67300E9F703BAD1E36E4FC164FBB558"
 
 ws_url = "wss://real.okex.com:10441/websocket"
+send_data = ['''{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker'}''',
+             '''{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_depth'}''']
 
 
 def on_message(ws, message):
-    print "message:", message
+    print "message:", type(message), message
 
 
 def on_error(ws, error):
@@ -22,26 +24,27 @@ def on_error(ws, error):
     on_reconnection()
 
 
-def on_reconnection(ws):
+def on_reconnection():
     run()
 
 
 def on_close(ws):
-    # ws.close()
+    ws.close()
     print "close!"
 
 
 def on_open(ws):
     print "enter on_open!"
     def run(*args):
-        # for item in range(3):
-        # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_usd_btc_depth'}'''
-        # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_ltc_btc_ticker'}'''
-        send_data = '''{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker'}'''
-        print "data:", send_data
-        ws.send(send_data)
-        print "send success: ", send_data
-        time.sleep(1)
+        for item in send_data:
+            # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_usd_btc_depth'}'''
+            # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_ltc_btc_ticker'}'''
+            # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_ticker'}'''
+            # send_data = '''{'event':'addChannel','channel':'ok_sub_spot_btc_usdt_depth'}'''
+            send_channel = item
+            print "data:", send_channel
+            ws.send(send_channel)
+            print "send success: ", send_channel
         # ws.close()
         print "Thread terminating..."
     Thread(target=run).start()
